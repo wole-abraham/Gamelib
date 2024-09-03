958890fad0b0b323764f0d2589bda4d67c56dc03
abraham <wole1702@gmail.com>
2024-09-03 14:08:38 +0100

    set-api_key_env

--- api_requests.py
+++ api_requests.py
@@ -1,15 +1,16 @@
 """ consumes and stores data """
 
 import requests
+import os
 import json
 from datetime import datetime, timedelta
 today = datetime.now()
 last_month = today - timedelta(days=30)
 
 date_range = f"{last_month.strftime('%Y-%m-%d')},{today.strftime('%Y-%m-%d')}"
-
+key = os.getenv('API_KEY')
 base_url = "https://api.rawg.io/api/games"
-api_key = {"key": "4f87adae5f3c405fbd2e86a44af08ec9", 
+api_key = {"key": key, 
            'dates': date_range,
            'ordering': '-added',  # Order by popularity (most added)
            
