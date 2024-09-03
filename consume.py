591fa6c3ddda8470bf4483ca4ca08a8d8efbe098
abraham <wole1702@gmail.com>
2024-09-03 15:04:12 +0100

    fixe

--- consume.py
+++ consume.py
@@ -3,13 +3,14 @@
 import requests
 import json
 from datetime import datetime, timedelta
+import os
 today = datetime.now()
 last_month = today - timedelta(days=30)
 
 
-
+key = os.getenv("API_KEY")
 base_url = "https://api.rawg.io/api/games/3498"
-api_key = {"key": "4f87adae5f3c405fbd2e86a44af08ec9"}
+api_key = {"key": key}
 
 req = requests.get(base_url, params=api_key)
 print(req.json())
